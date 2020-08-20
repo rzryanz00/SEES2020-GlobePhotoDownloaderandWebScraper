4cf5d6ea7617c4ffb05e3c7808b31a7cdd783c79
rzryanz00 <68756716+rzryanz00@users.noreply.github.com>
2020-08-19 23:52:26 -0400

    Update GlobePhotoDownloader.py

--- GlobePhotoDownloader.py
+++ GlobePhotoDownloader.py
@@ -20,6 +20,10 @@ EndDate = '2020-07-23'
 # 3. what is the name of the csv file? *keep quotes
 # Warning: Some computers hide the '.csv' ending of the file name. Adding an extra .csv will cause the program to fail.
 csvname = 'useridlist.csv'
+#Input your desired Image Resolution. ImageResolution = (Width in Pixels),(height in Pixels)
+ImageResolution = 1920, 1080
+#Would you like to start with UserID or SiteName? Set the variable to 0 for UserID and 1 for SiteName
+NameStart = 0
 
 #open the csv file containing the User IDs
 with open(csvname, 'r') as userID_file:
@@ -51,10 +55,10 @@ with open(csvname, 'r') as userID_file:
 
       #pick out the needed information and store them
       for landcover in data['results']:
-        siteId = landcover['siteId']
+        siteName = landcover['siteName']
         measuredDate = landcover['measuredDate']
-        latitude = landcover['latitude']
-        longitude = landcover['longitude']
+        latitude = landcover['data']['landcoversMeasurementLatitude']
+        longitude = landcover['data']['landcoversMeasurementLongitude']
         protocol = landcover['protocol']
         MUC = landcover['data']['landcoversMucCode']
         UpURL = landcover['data']['landcoversUpwardPhotoUrl']
@@ -73,14 +77,27 @@ with open(csvname, 'r') as userID_file:
 
         # convert the bytes to image
         image = Image.open(io.BytesIO(image_bytes))
+
+        #get the original size of the image
         width, height = image.size
-        # name and save it. Naming convention: GlobeObserver_UserID_SiteID_Protocol_latitude_longitude_MeasuredDate_MUC_Direction_WidthofPicture_HeightofPicture.jpg
-        image.save('GLOBEObserver_' + str(userid) + '_' + str(siteId) + '_' + protocol + '_' + str(latitude) + '_' + str(
+
+        #resize the image using the user designated resolution
+        ResizedImage = image.resize(ImageResolution, Image.ANTIALIAS)
+
+        #Change the order of UserID or SiteName depending on user preference
+        if NameStart == 0:
+          FirstinName = userid
+          SecondinName = siteName
+        else:
+          FirstinName = siteName
+          SecondinName = userid
+        # name and save it. Naming convention: GlobeObserver_(UserID or SiteName)_(SiteName or UserID)_Protocol_latitude_longitude_MeasuredDate_MUC_Direction_OriginalWidthofPicture_OriginalHeightofPicture_(ResizedWidth, ResizedHeight).jpg
+        ResizedImage.save('GLOBEObserver_' + str(FirstinName) + '_' + str(SecondinName) + '_' + protocol + '_' + str(latitude) + '_' + str(
           longitude) + '_' + str(measuredDate) + '_' + str(MUC) + '_' + 'East' + '_' + str(width) + 'x' + str(
-          height) + '.jpg')
+          height) + '_' + str(ImageResolution) + '.jpg')
 
         #write the name into the 'GlobeDownloadedPhotoNames' csv file
-        NewCSVWriter.writerow(['GLOBEObserver_' + str(userid) + '_' + str(siteId) + '_' + protocol + '_' + str(latitude) + '_' + str(
+        NewCSVWriter.writerow(['GLOBEObserver_' + str(FirstinName) + '_' + str(SecondinName) + '_' + protocol + '_' + str(latitude) + '_' + str(
           longitude) + '_' + str(measuredDate) + '_' + str(MUC) + '_' + 'East' + '_' + str(width) + 'x' + str(
           height) + '.jpg'])
 
@@ -90,13 +107,14 @@ with open(csvname, 'r') as userID_file:
         image_bytes = GetBytesfromURL.content
         image = Image.open(io.BytesIO(image_bytes))
         width, height = image.size
-        image.save('GLOBEObserver_' + str(userid) + '_' + str(siteId) + '_' + protocol + '_' + str(latitude) + '_' + str(
+        ResizedImage = image.resize(ImageResolution, Image.ANTIALIAS)
+        ResizedImage.save('GLOBEObserver_' + str(FirstinName) + '_' + str(SecondinName) + '_' + protocol + '_' + str(latitude) + '_' + str(
           longitude) + '_' + str(measuredDate) + '_' + str(MUC) + '_' + 'West' + '_' + str(width) + 'x' + str(
-          height) + '.jpg')
+          height) + '_' + str(ImageResolution) + '.jpg')
 
         # write the name into the 'GlobeDownloadedPhotoNames' csv file
         NewCSVWriter.writerow(
-          ['GLOBEObserver_' + str(userid) + '_' + str(siteId) + '_' + protocol + '_' + str(latitude) + '_' + str(
+          ['GLOBEObserver_' + str(FirstinName) + '_' + str(SecondinName) + '_' + protocol + '_' + str(latitude) + '_' + str(
             longitude) + '_' + str(measuredDate) + '_' + str(MUC) + '_' + 'West' + '_' + str(width) + 'x' + str(
             height) + '.jpg'])
 
@@ -105,13 +123,14 @@ with open(csvname, 'r') as userID_file:
         image_bytes = GetBytesfromURL.content
         image = Image.open(io.BytesIO(image_bytes))
         width, height = image.size
-        image.save('GLOBEObserver_' + str(userid) + '_' + str(siteId) + '_' + protocol + '_' + str(latitude) + '_' + str(
+        ResizedImage = image.resize(ImageResolution, Image.ANTIALIAS)
+        ResizedImage.save('GLOBEObserver_' + str(FirstinName) + '_' + str(SecondinName) + '_' + protocol + '_' + str(latitude) + '_' + str(
           longitude) + '_' + str(measuredDate) + '_' + str(MUC) + '_' + 'North' + '_' + str(width) + 'x' + str(
-          height) + '.jpg')
+          height) + '_' + str(ImageResolution) + '.jpg')
 
         # write the name into the 'GlobeDownloadedPhotoNames' csv file
         NewCSVWriter.writerow(
-          ['GLOBEObserver_' + str(userid) + '_' + str(siteId) + '_' + protocol + '_' + str(latitude) + '_' + str(
+          ['GLOBEObserver_' + str(FirstinName) + '_' + str(SecondinName) + '_' + protocol + '_' + str(latitude) + '_' + str(
             longitude) + '_' + str(measuredDate) + '_' + str(MUC) + '_' + 'North' + '_' + str(width) + 'x' + str(
             height) + '.jpg'])
 
@@ -120,13 +139,14 @@ with open(csvname, 'r') as userID_file:
         image_bytes = GetBytesfromURL.content
         image = Image.open(io.BytesIO(image_bytes))
         width, height = image.size
-        image.save('GLOBEObserver_' + str(userid) + '_' + str(siteId) + '_' + protocol + '_' + str(latitude) + '_' + str(
+        ResizedImage = image.resize(ImageResolution, Image.ANTIALIAS)
+        ResizedImage.save('GLOBEObserver_' + str(FirstinName) + '_' + str(SecondinName) + '_' + protocol + '_' + str(latitude) + '_' + str(
           longitude) + '_' + str(measuredDate) + '_' + str(MUC) + '_' + 'South' + '_' + str(width) + 'x' + str(
-          height) + '.jpg')
+          height) + '_' + str(ImageResolution) + '.jpg')
 
         # write the name into the 'GlobeDownloadedPhotoNames' csv file
         NewCSVWriter.writerow(
-          ['GLOBEObserver_' + str(userid) + '_' + str(siteId) + '_' + protocol + '_' + str(latitude) + '_' + str(
+          ['GLOBEObserver_' + str(FirstinName) + '_' + str(SecondinName) + '_' + protocol + '_' + str(latitude) + '_' + str(
             longitude) + '_' + str(measuredDate) + '_' + str(MUC) + '_' + 'South' + '_' + str(width) + 'x' + str(
             height) + '.jpg'])
 
@@ -135,13 +155,14 @@ with open(csvname, 'r') as userID_file:
         image_bytes = GetBytesfromURL.content
         image = Image.open(io.BytesIO(image_bytes))
         width, height = image.size
-        image.save('GLOBEObserver_' + str(userid) + '_' + str(siteId) + '_' + protocol + '_' + str(latitude) + '_' + str(
+        ResizedImage = image.resize(ImageResolution, Image.ANTIALIAS)
+        ResizedImage.save('GLOBEObserver_' + str(FirstinName) + '_' + str(SecondinName) + '_' + protocol + '_' + str(latitude) + '_' + str(
           longitude) + '_' + str(measuredDate) + '_' + str(MUC) + '_' + 'Upwards' + '_' + str(width) + 'x' + str(
-          height) + '.jpg')
+          height) + '_' + str(ImageResolution) + '.jpg')
 
         # write the name into the 'GlobeDownloadedPhotoNames' csv file
         NewCSVWriter.writerow(
-          ['GLOBEObserver_' + str(userid) + '_' + str(siteId) + '_' + protocol + '_' + str(latitude) + '_' + str(
+          ['GLOBEObserver_' + str(FirstinName) + '_' + str(SecondinName) + '_' + protocol + '_' + str(latitude) + '_' + str(
             longitude) + '_' + str(measuredDate) + '_' + str(MUC) + '_' + 'Upwards' + '_' + str(width) + 'x' + str(
             height) + '.jpg'])
 
@@ -150,12 +171,13 @@ with open(csvname, 'r') as userID_file:
         image_bytes = GetBytesfromURL.content
         image = Image.open(io.BytesIO(image_bytes))
         width, height = image.size
-        image.save('GLOBEObserver_' + str(userid) + '_' + str(siteId) + '_' + protocol + '_' + str(latitude) + '_' + str(
+        ResizedImage = image.resize(ImageResolution, Image.ANTIALIAS)
+        ResizedImage.save('GLOBEObserver_' + str(FirstinName) + '_' + str(SecondinName) + '_' + protocol + '_' + str(latitude) + '_' + str(
           longitude) + '_' + str(measuredDate) + '_' + str(MUC) + '_' + 'Downwards' + '_' + str(width) + 'x' + str(
-          height) + '.jpg')
+          height) + '_' + str(ImageResolution) + '.jpg')
 
         # write the name into the 'GlobeDownloadedPhotoNames' csv file
         NewCSVWriter.writerow(
-          ['GLOBEObserver_' + str(userid) + '_' + str(siteId) + '_' + protocol + '_' + str(latitude) + '_' + str(
+          ['GLOBEObserver_' + str(FirstinName) + '_' + str(SecondinName) + '_' + protocol + '_' + str(latitude) + '_' + str(
             longitude) + '_' + str(measuredDate) + '_' + str(MUC) + '_' + 'Downwards' + '_' + str(width) + 'x' + str(
             height) + '.jpg'])
